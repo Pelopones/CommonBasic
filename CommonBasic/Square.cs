e412a653b529aa817cc0ed530e9b48c488974d67
Pelopones <dv.taranov@gmail.com>
2015-11-12 18:51:09 +0300

    Создание функции для расчета площади прямоугольного треугольника. Станет частью библиотеки поставляемой внешним клиентам.

---
+++ CommonBasic/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonBasic
+{
+    public class Square
+    {
+        /*Площадь прямоугольного треугольника*/
+        public static double RectangularTriangle(double a, double b)
+        {
+            if (a <= 0 || b <= 0)
+            {
+                return 0;
+            }
+
+            return a * b / 2.0; 
+        }
+    }
+}
---
+++ UnitTest/SquareTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonBasic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class SquareTests
+    {
+        // unit test code
+        [TestMethod]
+        public void RectangularTriangle()
+        {
+            // angle
+            double a = 2, b = 3, SValid = 3;
+
+            // assert
+            Assert.AreEqual(SValid, Square.RectangularTriangle(a, b), "Square rectangular triangle not correctly");
+        }
+
+        [TestMethod]
+        public void RectangularTriangleOutOfRange()
+        {
+            // angle
+            double a = -6, b = 3, SValid = 0;
+
+            // assert
+            Assert.AreEqual(SValid, Square.RectangularTriangle(a, b), "Square rectangular triangle not correctly");
+        }
+    }
+}
